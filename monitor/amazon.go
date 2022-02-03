fb1e8a9fb77fefa04d6218fd721bde6dfa71128d
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-03 16:37:03 -0500

    Update amazon.go

--- monitor/amazon.go
+++ monitor/amazon.go
@@ -1,13 +1,22 @@
 package monitor
 
 import (
+	"elektra/checkout"
+	"fmt"
 	"github.com/wux1an/fake-useragent"
 	"github.com/obito/cclient"
 	utls "github.com/refraction-networking/utls"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"elektra"
+	"strings"
+	"time"
 )
 
 
-func amazonCheckStock(monitorData *AmazonMonitorData) (bool, bool) {
+func amazonCheckStock(client *http.Client, monitorData *elektra.AmazonMonitorData, apiToken string) (bool, bool) {
 	acceptheader := "application/vnd.com.amazon.api+json; type=\"cart.add-items/v1\""
 	contentheader := "application/vnd.com.amazon.api+json; type=\"cart.add-items.request/v1\""
 	
@@ -17,7 +26,7 @@ func amazonCheckStock(monitorData *AmazonMonitorData) (bool, bool) {
 		log.Fatal(err)
 	}
 	
-	req.Header.Set("x-api-csrf-token", apitoken)
+	req.Header.Set("x-api-csrf-token", apiToken)
 	req.Header.Set("Content-Type", contentheader)
 	req.Header.Set("Accept", acceptheader)
 	req.Header.Set("User-Agent", monitorData.UserAgent)
@@ -36,7 +45,7 @@ func amazonCheckStock(monitorData *AmazonMonitorData) (bool, bool) {
 	return false, false //Usually status code 422 (out of stock) but an api token refresh is not required
 }
 
-func getApiToken(client *http.Client) string {
+func getApiToken(client *http.Client, monitorData *elektra.AmazonMonitorData) string {
   	url := "https://www.amazon.com/gp/aw/d/B00M382RJO" //One of many Amazon product pages that contains an embedded api token
   
 	req, err := http.NewRequest("GET", url, nil)
@@ -50,11 +59,11 @@ func getApiToken(client *http.Client) string {
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	
-	apiToken := Parse(string(body), "\"csrfToken\":\"", "\",\"baseAsin\"")
+	apiToken := checkout.Parse(string(body), "\"csrfToken\":\"", "\",\"baseAsin\"")
 	return apiToken
 }
 
-func createSession(client *http.Client) {
+func createSession(client *http.Client, monitorData *elektra.AmazonMonitorData) {
 	url := "https://www.amazon.com/gp/aws/cart/add-res.html?Quantity.1=1&OfferListingId.1="
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -72,42 +81,42 @@ func createSession(client *http.Client) {
 	return string(sessionid)*/
 }
 
-func AmazonMonitorTask(monitorData *AmazonMonitorData) {
+func AmazonMonitorTask(monitorData *elektra.AmazonMonitorData) {
 	var client *http.Client
 	if monitorData.UseProxies {
 		rand.Seed(time.Now().Unix())
 		proxy := "http://" + monitorData.Proxies[rand.Intn(len(monitorData.Proxies))] //Only works with IP authenticated proxies atm (IP:Port), not yet with User:Pass:IP:Port proxies
 		
-		client, err = cclient.NewClient(utls.HelloFirefox_Auto, true, proxy) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true, and use a proxy
+		client, err := cclient.NewClient(utls.HelloFirefox_Auto, true, proxy) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true, and use a proxy
+		_ = client
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		client, err = cclient.NewClient(utls.HelloFirefox_Auto, true) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true
+		client, err := cclient.NewClient(utls.HelloFirefox_Auto, true) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true
+		_ = client
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	
-	var userAgent string
-	
+
 	if monitorData.UserAgent == "" {
 		monitorData.UserAgent = ua.RandomType(ua.Desktop)
 	}
 	
-	createSession(&client, userAgent)
-	apiToken := getApiToken(&client, userAgent)
+	createSession(client, monitorData)
+	apiToken := getApiToken(client, monitorData)
 	
 	for {
-		inStock, refreshRequired := amazonCheckStock(&client, &monitorData, userAgent)
+		inStock, refreshRequired := amazonCheckStock(client, monitorData, apiToken)
 		if inStock {
 			return 
 		} else {
 			if refreshRequired {
-				apiToken = getApiToken(&client)
+				apiToken = getApiToken(client, monitorData)
 			}
 		}
 		
-		time.Sleep(time.Second * time.Interval(monitorData.PollingInterval))
+		time.Sleep(time.Second * time.Duration(monitorData.PollingInterval))
 	}
 }
