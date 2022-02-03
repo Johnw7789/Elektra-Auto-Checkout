21e2cf64f26fce4859f9837412475a170ec54be6
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-03 18:26:41 -0500

    Added Parse and CreateClient functions

--- elektra/elektra.go
+++ elektra/elektra.go
@@ -1,5 +1,15 @@
 package elektra
 
+import (
+	"github.com/obito/cclient"
+	utls "github.com/refraction-networking/utls"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
 type BillingData struct {
 	Email     string
 	Phone     string
@@ -56,4 +66,41 @@ type BestbuyMonitorData struct {
 type NeweggMonitorData struct {
 	Delay int
 	Sku   string
+}
+
+func Parse(value string, a string, b string) string {
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
+}
+
+func CreateClient(useProxies bool, proxies []string) *http.Client {
+	if useProxies {
+		rand.Seed(time.Now().Unix())
+		proxy := "http://" + proxies[rand.Intn(len(proxies))] //Only works with IP authenticated proxies atm (IP:Port), not yet with User:Pass:IP:Port proxies
+
+		client, err := cclient.NewClient(utls.HelloFirefox_Auto, true, proxy) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true, and use a proxy
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return &client
+	} else {
+		client, err := cclient.NewClient(utls.HelloFirefox_Auto, true) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return &client
+	}
 }
