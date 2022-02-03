2c5dd82e44516f66f8bc81275193bb1eb5666833
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-03 11:55:19 -0500

    Update amazon.go

--- monitor/amazon.go
+++ monitor/amazon.go
@@ -22,20 +22,19 @@ func checkStock() (bool, bool) {
 	}
 	
 	if resp.StatusCode == 200 {
-		return true, false //In stock
+		return true, false //In stock, api token refresh is not required
 	} else if resp.StatusCode == 404 {
 		return false, true //Out of stock, but an api token refresh is required 
 	} 
 	
-	return false, false //Out of stock and an api token refresh is not required
+	return false, false //Usually status code 422 (out of stock) but an api token refresh is not required
 }
 
-func getApiToken() {
+func getApiToken(client *http.Client) string {
   	url := "https://www.amazon.com/gp/aw/d/B00M382RJO" //One of many Amazon product pages that contains an embedded api token
   
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
-	req.Header.Set("Cookie", "session-id=" + sessionid)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -44,11 +43,12 @@ func getApiToken() {
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	apitoken := Parse(string(body), "\"csrfToken\":\"", "\",\"baseAsin\"")
-	return apitoken
+	
+	apiToken := Parse(string(body), "\"csrfToken\":\"", "\",\"baseAsin\"")
+	return apiToken
 }
 
-func getSessionId(client *http.Client) {
+func createSession(client *http.Client) {
 	url := "https://www.amazon.com/gp/aws/cart/add-res.html?Quantity.1=1&OfferListingId.1="
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -61,11 +61,30 @@ func getSessionId(client *http.Client) {
 	defer resp.Body.Close()
 
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	/*body, _ := ioutil.ReadAll(resp.Body)
 	sessionid := (Parse(string(body), "ue_sid='", "',\nue_mid='"))
-	return string(sessionid)
+	return string(sessionid)*/
 }
 
 func Amazon() {
+	client, err := cclient.NewClient(utls.HelloFirefox_Auto, true) //Create an http client with a Firefox TLS fingerprint, set automatic storage of cookies to true
+	if err != nil {
+		log.Fatal(err)
+	}
 	
+	createSession(&client)
+	apiToken := getApiToken(&client)
+	
+	for {
+		inStock, refreshRequired := checkStock(&client, apiToken)
+		if inStock {
+			return 
+		} else {
+			if refreshRequired {
+				apiToken = getApiToken(&client)
+			}
+		}
+		
+		time.Sleep(time.Second * time.Interval())
+	}
 }
