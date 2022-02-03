a8a7af708245f014b24d9a8e011a174846d1cab2
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-03 11:32:45 -0500

    Update amazon.go

--- monitor/amazon.go
+++ monitor/amazon.go
@@ -3,11 +3,11 @@ package monitor
 var turboHeaders = []string{
 	"Accept: */*", 
 	"Content-Type: application/x-www-form-urlencoded",
-  "x-amz-support-custom-signin: 1",
-  "x-requested-with: XMLHttpRequest",
-  "accept-language: en-US,en;q=0.9",
-  "origin: https://www.amazon.com",
-  "referer: https://www.amazon.com",
+	"x-amz-support-custom-signin: 1",
+  	"x-requested-with: XMLHttpRequest",
+  	"accept-language: en-US,en;q=0.9",
+  	"origin: https://www.amazon.com",
+  	"referer: https://www.amazon.com",
 }
 
 
@@ -29,7 +29,34 @@ func Parse(value string, a string, b string) {
 }
 
 func placeOrder() {
-
+	var data = strings.NewReader(`x-amz-checkout-csrf-token=` + accountSessionId + `&ref_=chk_spc_placeOrder&referrer=spc&pid=` + purchaseId + `&pipelineType=turbo&clientId=retailwebsite&temporaryAddToCart=1&hostPage=detail&weblab=RCX_CHECKOUT_TURBO_DESKTOP_PRIME_87783&isClientTimeBased=1`)
+	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/spc/place-order?ref_=chk_spc_placeOrder&_srcRID=&clientId=retailwebsite&pipelineType=turbo&cachebuster=&pid=" + purchaseId, data)
+	if err != nil {
+		return false
+	}
+	
+	req.Header.Set("x-amz-checkout-entry-referer-url", "https://" + domain + ".amazon.com/gp/product/" + productId + "/ref=ewc_pr_img_1?smid=AZ5LJ56P0QUDV&psc=1")
+	req.Header.Set("anti-csrftoken-a2z", csrfToken)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
+	req.Header.Set("Referer", "https://www.amazon.com/checkout/spc?pid=" + purchaseId + "&pipelineType=turbo&clientId=retailwebsite&temporaryAddToCart=1&hostPage=detail&weblab=RCX_CHECKOUT_TURBO_DESKTOP_PRIME_87783")
+	req.Header.Add("cookie", cookies)
+	resp, err := client.Do(req)
+	if err != nil {
+		return false
+	}
+	
+	defer resp.Body.Close()
+	
+	for key, value := range resp.Header {
+		if strings.Contains(key, "thankyou") || strings.Contains(value[0], "thankyou") {
+			return true
+		}
+	}
+	
+	if 
+	
+	fmt.Println(fmt.Sprintf("[%s] [Amazon Turbo V2] [SKU %s] %s", time.Now().Format("15:04:05"), productId, red("Order Failed")))
+	return false
 }
 
 func addToCart(client *http.Client) (bool, string, string) {
@@ -43,18 +70,21 @@ func addToCart(client *http.Client) (bool, string, string) {
 
 	req.Header.Set("x-amz-checkout-entry-referer-url", "https://" + domain + ".amazon.com/dp/" + productId)
 	req.Header.Set("x-amz-turbo-checkout-dp-url", "https://" + domain + ".amazon.com/dp/" + productId)
-  req.Header.Set("x-amz-checkout-csrf-token", accountSessionId)
+  	req.Header.Set("x-amz-checkout-csrf-token", accountSessionId)
 	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
 	req.Header.Set("cookie", cookies)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	defer resp.Body.Close()
+	
 	bodyText, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	if strings.Contains(string(bodyText), "Place your order") {
 		doc := soup.HTMLParse(string(bodyText))
 		purchaseId := Parse(string(bodyText), "currentPurchaseId\":\"", "\",\"pipelineType\"")
@@ -66,7 +96,7 @@ func addToCart(client *http.Client) (bool, string, string) {
 }
 
 func getApiToken() {
-  url := "https://www.amazon.com/gp/aw/d/B00M382RJO" //One of many Amazon product pages that contains an embedded api token
+  	url := "https://www.amazon.com/gp/aw/d/B00M382RJO" //One of many Amazon product pages that contains an embedded api token
   
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
@@ -102,5 +132,5 @@ func getSessionId(client *http.Client) {
 }
 
 func Amazon() {
-  
+	
 }
