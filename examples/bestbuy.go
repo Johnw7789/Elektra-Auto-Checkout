c38d2c665aae7208d9e5d29eacacaadea9809372
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-04 00:09:01 -0500

    Update bestbuy.go

--- examples/bestbuy.go
+++ examples/bestbuy.go
@@ -1,5 +1,23 @@
 package examples
 
-func Bestbuy() {
+import (
+	"fmt"
+	"github.com/ffeathers/Elektra-Auto-Checkout/elektra"
+	"github.com/ffeathers/Elektra-Auto-Checkout/monitor"
+	"log"
+)
 
+func main() {
+	monitorData := elektra.AmazonMonitorData{
+		UserAgent:       "",
+		UseProxies:      false,
+		Proxies:         []string{"IP:Port", "IP:Port"},
+		PollingInterval: 3,
+		Sku:             "B0873C4C67",
+		OfferId:         "5%2BU3RbI4MrLxJP1riew3ktYPNAEuKmceCPF1BTaKdwF9bGnxPX3cfIChUFRKBusiTPTd3gJEB9Az0V3TlZw0po6Mob%2BYvq37tir2AWHORVYNxN9kBTPxMuvTkuiELMuz3q9BWdzZKsylbBhRmq7cAHQgq7p9VSdR5e6J%2BWxORLR95D2He%2BodtT4wtctu24wt",
+	}
+
+	monitor.AmazonMonitorTask(&monitorData)
+
+	log.Println(fmt.Sprintf("SKU %s: In Stock", monitorData.Sku))
 }
