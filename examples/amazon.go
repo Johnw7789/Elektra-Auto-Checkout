a34572b91fe64c4a51381c31a055d78e672472a6
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-04 00:10:13 -0500

    Update amazon.go

--- examples/amazon.go
+++ examples/amazon.go
@@ -11,7 +11,6 @@ func main() {
 	monitorData := elektra.AmazonMonitorData{
 		UserAgent:       "",
 		UseProxies:      false,
-		Proxies:         []string{"IP:Port", "IP:Port"},
 		PollingInterval: 3,
 		Sku:             "B0873C4C67",
 		OfferId:         "5%2BU3RbI4MrLxJP1riew3ktYPNAEuKmceCPF1BTaKdwF9bGnxPX3cfIChUFRKBusiTPTd3gJEB9Az0V3TlZw0po6Mob%2BYvq37tir2AWHORVYNxN9kBTPxMuvTkuiELMuz3q9BWdzZKsylbBhRmq7cAHQgq7p9VSdR5e6J%2BWxORLR95D2He%2BodtT4wtctu24wt",
