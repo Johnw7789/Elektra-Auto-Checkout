64b7e3c0a27afcc050eafdcd7826c267774c7a0b
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-04 00:42:03 -0500

    Update newegg.go

--- examples/newegg.go
+++ examples/newegg.go
@@ -1,5 +1,21 @@
-package login
+package examples
 
-func NeweggLoginSession() {
+import (
+	"fmt"
+	"github.com/ffeathers/Elektra-Auto-Checkout/elektra"
+	"github.com/ffeathers/Elektra-Auto-Checkout/monitor"
+	"log"
+)
 
+func main() {
+	monitorData := elektra.NeweggMonitorData{
+		UserAgent:       "",
+		UseProxies:      false,
+		PollingInterval: 3,
+		Sku:             "N82E16835181166",
+	}
+
+	monitor.NeweggMonitorTask(&monitorData)
+
+	log.Println(fmt.Sprintf("SKU %s: In Stock", monitorData.Sku))
 }
