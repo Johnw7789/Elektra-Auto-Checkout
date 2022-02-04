d2188ebc2bcf65edf21266b9dcd50a859964d131
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-04 00:37:05 -0500

    Update newegg.go

--- monitor/newegg.go
+++ monitor/newegg.go
@@ -12,30 +12,20 @@ import (
 )
 
 func neweggCheckStock(client *http.Client, monitorData *elektra.NeweggMonitorData) bool {
-  	req, err := http.NewRequest("GET", "https://www.bestbuy.com/button-state/api/v5/button-state?skus=" + monitorData.Sku + "&context=pdp&source=buttonView", nil)
+  	req , err := http.NewRequest("GET", "https://www.newegg.com/product/api/ProductRealtime?ItemNumber=" + productId, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Set("authority", "www.bestbuy.com")
-	req.Header.Set("host", "www.bestbuy.com")
-	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64")
+	
+	req.Header.Set("user-agent", monitorDataUserAgent)
 	req.Header.Set("accept", "*/*")
-	req.Header.Set("x-client-id", "FRV")
-	req.Header.Set("Connection", "keep-alive")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
   
-	if resp.StatusCode == 200 {
-    		bodyText, _ := ioutil.ReadAll(resp.Body)
-    		if strings.Contains(string(bodyText), "CHECK_STORES") || strings.Contains(string(bodyText), "ADD_TO_CART") {
-      			return true
-		}
-  	} else {
-   		 log.Println(fmt.Sprintf("Status Code: %d", resp.StatusCode))
-  	}
+	//Todo: check if in stock
   
   	return false
 }
