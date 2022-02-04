ba5eddcdfcec6cc3e3d9a99dd0ecaf8f4a338816
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-04 00:38:50 -0500

    Create types.go

---
+++ elektra/types.go
@@ -0,0 +1,69 @@
+package elektra
+
+type BillingData struct {
+	Email     string
+	Phone     string
+	FirstName string
+	LastName  string
+	State     string
+	City      string
+	ZipCode   string
+	Address1  string
+	Address2  string
+	CardType  string
+	CardNum   string
+	ExpMonth  string
+	ExpYear   string
+	CardName  string
+	Cvv       string
+}
+
+type AccountData struct {
+	Username string
+	Password string
+	Email    string
+	Phone    string
+}
+
+type AmazonMonitorData struct {
+	UserAgent       string
+	Proxies         []string
+	UseProxies      bool
+	PollingInterval int
+	Sku             string
+	OfferId         string
+}
+
+type AmazonCheckoutData struct {
+	UserAgent  string
+	SessionId  string
+	Cookies    string
+	Proxies    []string
+	UseProxies bool
+	RetryDelay int
+	MaxRetries int
+	Sku        string
+	OfferId    string
+	OrderNum   string
+}
+
+type BestbuyMonitorData struct {
+	UserAgent       string
+	Proxies         []string
+	UseProxies      bool
+	PollingInterval int
+	Sku             string
+}
+
+type NeweggMonitorData struct {
+	UserAgent       string
+	Proxies         []string
+	UseProxies      bool
+	PollingInterval int
+	Sku             string
+}
+
+type NeweggMonitorData struct {
+	Delay int
+	Sku   string
+}
