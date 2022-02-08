349b2e37c8ac45e16c751b23fa616871d67e1c8f
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-08 12:04:53 -0500

    Updated example with error handling

--- examples/amazon.go
+++ examples/amazon.go
@@ -11,33 +11,40 @@ func main() {
 	cookieString := ""
 	sku := "B0873C4C67"
 	offerId := "5%2BU3RbI4MrLxJP1riew3ktYPNAEuKmceCPF1BTaKdwF9bGnxPX3cfIChUFRKBusiTPTd3gJEB9Az0V3TlZw0po6Mob%2BYvq37tir2AWHORVYNxN9kBTPxMuvTkuiELMuz3q9BWdzZKsylbBhRmq7cAHQgq7p9VSdR5e6J%2BWxORLR95D2He%2BodtT4wtctu24wt"
-	
-	monitorData := elektra.AmazonMonitorData{
-		UserAgent:       "",
-		UseProxies:      false,
+		
+	monitor := monitor.AmazonMonitor{
+		UserAgent: "",
+		Proxy: "",
 		PollingInterval: 3,
-		Sku:             sku,
-		OfferId:         offerid,
+		Sku: "ASIN",
+		OfferId: "OfferId",
 	}
 
-	monitor.AmazonMonitorTask(&monitorData)
-
-	log.Println(fmt.Sprintf("SKU %s: In Stock, Initiating Checkout", monitorData.Sku))
-	
-	
-	checkoutData := elektra.AmazonCheckoutData{
-  		UserAgent: "",
-  		UseProxies: true,
- 		Proxies: []string{"IP:Port", "IP:Port"},
-  		Cookies: cookieString,
-  		MaxRetries: 5,
-  		RetryDelay: 3,
-  		Sku: sku,
- 		OfferId: offerId,
+	banned, err := monitor.AmazonMonitorTask()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	if banned {
+		log.Println("Your IP was flagged")
+	} else {
+		log.Println(fmt.Sprintf("SKU %s: In Stock, Initiating Checkout", monitorData.Sku))
+		
+			
+		checkoutData := elektra.AmazonCheckoutData{
+  			UserAgent: "",
+  			UseProxies: true,
+ 			Proxies: []string{"IP:Port", "IP:Port"},
+  			Cookies: cookieString,
+  			MaxRetries: 5,
+  			RetryDelay: 3,
+  			Sku: sku,
+ 			OfferId: offerId,
+		}
   
-	orderSuccess := checkout.AmazonCheckoutTask(&checkoutData) 
-	if orderSuccess {
-  		log.Println("Checkout successful | order number: " + checkoutData.OrderNum)
+		orderSuccess := checkout.AmazonCheckoutTask(&checkoutData) 
+		if orderSuccess {
+  			log.Println("Checkout successful | order number: " + checkoutData.OrderNum)
+		}
 	}
 }
