7e0e315a38c23210e4a9ff06a84a7f81e6388949
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-02-08 11:46:45 -0500

    Added error handling

--- monitor/newegg.go
+++ monitor/newegg.go
@@ -11,40 +11,61 @@ import (
 	"time"
 )
 
-func neweggCheckStock(client *http.Client, monitorData *elektra.NeweggMonitorData) bool {
-  	req , err := http.NewRequest("GET", "https://www.newegg.com/product/api/ProductRealtime?ItemNumber=" + monitorData.Sku, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	req.Header.Set("user-agent", monitorData.UserAgent)
-	req.Header.Set("accept", "*/*")
 
-	_, err = client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-  
-	//Todo: check if in stock
-  
-  	return false
+type NeweggMonitor struct {
+	UserAgent       string
+	Proxy           string
+	PollingInterval int
+	Sku             string
 }
 
-func NeweggMonitorTask(monitorData *elektra.NeweggMonitorData) {
-	client := elektra.CreateClient(monitorData.UseProxies, monitorData.Proxies)
+func (monitor *NeweggMonitor) neweggCheckStock (client *http.Client,) (bool, bool, error) {
+  	req , err := http.NewRequest("GET", "https://www.newegg.com/product/api/ProductRealtime?ItemNumber=" + monitor.Sku, nil)
+	if err != nil {
+		return false, false, nil
+	}
+	
+	req.Header.Set("user-agent", monitor.UserAgent)
+	req.Header.Set("accept", "*/*")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, false, nil
+	}
+
+	if resp.StatusCode == 200 {
+		//body, _ := ioutil.ReadAll(resp.Body)
+	} else {
+		return false, true, nil
+	}
+
+  	return false, false, nil
+}
+
+func (monitor *NeweggMonitor) NeweggMonitorTask() (bool, error) {
+	client, err := elektra.CreateClient(monitor.Proxy)
+	if err != nil {
+		return false, err
+	}
  
-	if monitorData.UserAgent == "" {
-		monitorData.UserAgent = ua.RandomType(ua.Desktop)
+	if monitor.UserAgent == "" {
+		monitor.UserAgent = ua.RandomType(ua.Desktop)
 	}
   
 	for {
 		log.Println("Checking Stock")
-		inStock := neweggCheckStock(client, monitorData)
-		if inStock {
-			return
+		inStock, isBanned, err := monitor.neweggCheckStock(client)
+		if err != nil {
+			return false, err
 		}
 
-		time.Sleep(time.Second * time.Duration(monitorData.PollingInterval))
+		if inStock {
+			return false, nil
+		} else if isBanned {
+			return true, nil
+		}
+
+		time.Sleep(time.Second * time.Duration(monitor.PollingInterval))
 	}
 }
 
