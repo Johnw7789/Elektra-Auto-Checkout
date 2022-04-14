890808942d11e26cb3637c9afb7ac35a5a0d299e
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-04-14 15:09:15 -0400

    Add to bestbuy login flow

--- login/bestbuy.go
+++ login/bestbuy.go
@@ -206,6 +206,67 @@ func (login *BestBuyLogin) bestbuyEncrypt(s string, publicKey string, keyId stri
 	return strings.Join([]string{"1", keyId, base64.StdEncoding.EncodeToString(ciphertext)}, ":")
 }
 
+
+func scrapeLoginData(client * http.Client) error {
+	req, err := http.NewRequest("GET", "https://www.bestbuy.com/identity/global/signin", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("authority", "www.bestbuy.com")
+	req.Header.Set("upgrade-insecure-requests", "1")
+	req.Header.Set("user-agent", login.UserAgent)
+	req.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("sec-fetch-site", "none")
+	req.Header.Set("sec-fetch-mode", "navigate")
+	req.Header.Set("sec-fetch-user", "?1")
+	req.Header.Set("sec-fetch-dest", "document")
+	req.Header.Set("sec-ch-ua", "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "\"macOS\"")
+	req.Header.Set("accept-language", "en-US,en;q=0.9")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+
+	responseString := string(response)
+
+	initDataBytes := Parse(responseString, "var initData = ", "; </script>")
+	initData := string(initDataBytes)
+
+	login.BestBuyLoginData.VerificationCodeFieldName := gjson.Get(initData, "verificationCodeFieldName").String()
+
+	passwordArray := gjson.Get(initData, "codeList")
+	for _, passwordField := range passwordArray.Array() {
+		decodedString, _ := base64.URLEncoding.DecodeString(passwordField.String())
+		if strings.Contains(string(decodedString) , "_X_") {
+			login.BestBuyLoginData.EncryptedPasswordField = passwordField.String()
+			break
+		}
+	}
+
+	alphaArray := gjson.Get(initData, "alpha")
+	for _, alpha := range alphaArray.Array() {
+		decodedString, _ := base64.URLEncoding.DecodeString(reverse(alpha.String()))
+		if strings.Contains(string(decodedString) , "_A_") {
+			login.BestBuyLoginData.EncryptedAlpha = alpha.String()
+			break
+		}
+	}
+
+	login.BestBuyLoginData.EmailField := gjson.Get(initData, "emailFieldName").String()
+	login.BestBuyLoginData.Salmon := gjson.Get(initData, "Salmon").String()
+	login.BestBuyLoginData.Token := gjson.Get(initData, "token").String()
+}
+
+
+
 func (login *BestBuyLogin) BestbuyLoginSession() (bool, bool, error) {
 	client, err := elektra.CreateClient(login.Proxy)
 	if err != nil {
@@ -217,7 +278,11 @@ func (login *BestBuyLogin) BestbuyLoginSession() (bool, bool, error) {
 		login.UserAgent = ua.RandomType(ua.Desktop)
 	}
 
-	login.scrapeLoginData(client)
+	err = login.scrapeLoginData(client)
+	if err != nil {
+		log.Println("Error scraping login data")
+		return false, false, err
+	}
 	
   	emailPublicKey, emailKeyId, err := getPublicKey(client, "https://www.bestbuy.com/api/csiservice/v2/key/cia-email")
 	if err != nil {
