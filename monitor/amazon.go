d48545106ea756ba3e751a72721d9d7168bb70b2
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-11-11 13:26:20 -0800

    Update amazon.go

--- monitor/amazon.go
+++ monitor/amazon.go
@@ -90,6 +90,13 @@ func (monitor *AmazonMonitor) AmazonCheckStockV2(client *http.Client, apiToken s
 	body, _ := ioutil.ReadAll(resp.Body)
 	result := gjson.Get(string(body), "entities.0")
 
+	if strings.Contains(result, "amount") {
+		price := gjson.Get(result, "entity.buyingOptions.0.price.entity.priceToPay.moneyValueOrRange.value.amount").Float()
+		if price <= monitor.Price {
+			//in stock because price shows up and is equal to or lower than limit
+			return true, false, false, nil
+		}
+	}
 
 	return false, false, true, nil
 }
