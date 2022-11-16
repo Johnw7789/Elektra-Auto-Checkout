67538d64722ff7cd3d2481749739354fb0704d3f
ffeathers <74395844+ffeathers@users.noreply.github.com>
2022-11-16 11:00:12 -0500

    TLS

--- checkout/amazon.go
+++ checkout/amazon.go
@@ -2,15 +2,16 @@ package checkout
 
 import (
 	"fmt"
-	"github.com/anaskhan96/soup"
-	"github.com/ffeathers/Elektra-Auto-Checkout/elektra"
-	"github.com/google/uuid"
-	ua "github.com/wux1an/fake-useragent"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"strings"
 	"time"
+
+	"github.com/anaskhan96/soup"
+	"github.com/google/uuid"
+	http "github.com/saucesteals/fhttp"
+	"github.com/saucesteals/mimic"
+	"golang.design/x/clipboard"
 )
 
 
@@ -27,17 +28,7 @@ type AmazonCheckout struct {
 	OrderNum   		string
 	LoggingDisabled 	bool
 	Active     	    	bool
-}
-
-
-var turboHeaders = []string{
-	"Accept: */*",
-	"Content-Type: application/x-www-form-urlencoded",
-	"x-amz-support-custom-signin: 1",
-	"x-requested-with: XMLHttpRequest",
-	"accept-language: en-US,en;q=0.9",
-	"origin: https://www.amazon.com",
-	"referer: https://www.amazon.com",
+	Mimicer         	mimic.ClientSpec
 }
 
 
@@ -53,25 +44,76 @@ func (checkout *AmazonCheckout) Cancel() {
 	//add exit code
 }
 
+func cookieHeader(rawCookies string) []*http.Cookie {
+	header := http.Header{}
+	header.Add("Cookie", rawCookies)
+	req := http.Request{Header: header}
+	return req.Cookies()
+}
 
-func (checkout *AmazonCheckout) amazonPlaceOrder(client *http.Client, purchaseId string, csrfToken string) (bool, string, bool, error) {
-	var data = strings.NewReader(fmt.Sprintf(`x-amz-checkout-csrf-token=%s&ref_=chk_spc_placeOrder&referrer=spc&pid=%s&pipelineType=turbo&clientId=retailwebsite&temporaryAddToCart=1&hostPage=detail&weblab=RCX_CHECKOUT_TURBO_DESKTOP_PRIME_87783&isClientTimeBased=1`, checkout.SessionId, purchaseId))
-	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/spc/place-order?ref_=chk_spc_placeOrder&_srcRID=&clientId=retailwebsite&pipelineType=turbo&cachebuster=&pid="+purchaseId, data)
+func (checkout *AmazonCheckout) AmazonPlaceOrderV1(client *http.Client, purchaseId string, csrfToken string) (bool, string, bool, error) {
+	var data = strings.NewReader(fmt.Sprintf(`x-amz-checkout-csrf-token=%s&ref_=chk_spc_placeOrder&referrer=spc&pid=%s&pipelineType=turbo&clientId=retailwebsite&hostPage=detail&isClientTimeBased=1`, checkout.SessionId, purchaseId))
+	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/spc/place-order?pipelineType=turbo", data)
 	if err != nil {
 		return false, "", false, err
 	}
 
-	req.Header.Set("x-amz-checkout-entry-referer-url", "https://www.amazon.com/gp/product/"+checkout.Sku)
-	req.Header.Set("anti-csrftoken-a2z", csrfToken)
-	req.Header.Set("User-Agent", checkout.UserAgent)
-	req.Header.Set("Referer", "https://www.amazon.com/checkout/spc?pid="+purchaseId+"&pipelineType=turbo&clientId=retailwebsite&temporaryAddToCart=1&hostPage=detail&weblab=RCX_CHECKOUT_TURBO_DESKTOP_PRIME_87783")
-	req.Header.Add("cookie", checkout.Cookies)
+	req.Header = http.Header{
+		"accept":                           {"*/*"},
+		"anti-csrftoken-a2z":               {csrfToken},
+		"accept-encoding":                  {"gzip, deflate, br"},
+		"accept-language":                  {"en,en_US;q=0.9"},
+		"content-length":                   {"184"},
+		"content-type":                     {"application/x-www-form-urlencoded"},
+		"cookie":                           {checkout.Cookies},
+		"device-memory":                    {"8"},
+		"downlink":                         {"3.9"},
+		"dpr":                              {"1"},
+		"ect":                              {"4g"},
+		"rtt":                              {"50"},
+		"sec-ch-device-memory":             {"8"},
+		"sec-ch-dpr":                       {"1"},
+		"sec-ch-ua":                        {checkout.Mimicer.ClientHintUA()},
+		"sec-ch-ua-mobile":                 {"?0"},
+		"sec-ch-ua-platform":               {`"Windows"`},
+		"sec-ch-viewport-width":            {`"988"`},
+		"sec-fetch-dest":                   {"empty"},
+		"sec-fetch-mode":                   {"cors"},
+		"sec-fetch-site":                   {"same-origin"},
+		"user-agent":                       {checkout.UserAgent},
+		"viewport-width":                   {"988"},
+		"x-amz-checkout-entry-referer-url": {"https://www.amazon.com/dp/" + checkout.Sku},
+		"x-requested-with":                 {"XMLHttpRequest"},
+
+		http.HeaderOrderKey: {
+			"accept", "anti-csrftoken-a2z", "accept-encoding", "accept-language",
+			"content-length", "content-type", "cookie",
+			"device-memory", "downlink", "dpr",
+			"ect", "rtt", "sec-ch-device-memory",
+			"sec-ch-dpr", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform",
+			"sec-ch-viewport-width", "sec-fetch-dest", "sec-fetch-mode",
+			"sec-fetch-site", "user-agent", "viewport-width", "x-amz-checkout-entry-referer-url",
+			"x-amz-support-custom-signin", "x-amz-turbo-checkout-dp-url",
+			"x-requested-with",
+		},
+		http.PHeaderOrderKey: checkout.Mimicer.PseudoHeaderOrder(),
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return false, "", false, err
 	}
 
 	defer resp.Body.Close()
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, "", false, err
+	}
+
+	_ = clipboard.Init()
+
+	clipboard.Write(clipboard.FmtText, []byte(string(bodyText)))
 
 	for key, value := range resp.Header {
 		if strings.Contains(key, "thankyou") || strings.Contains(value[0], "thankyou") {
@@ -79,32 +121,66 @@ func (checkout *AmazonCheckout) amazonPlaceOrder(client *http.Client, purchaseId
 		}
 	}
 
-	bodyText, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return false, "", false, err
-	}
-
-	if strings.Contains(string(bodyText), "thankyou") {
+	if strings.Contains(string(bodyText), "thankyou") || strings.Contains(string(bodyText), "please check to confirm that the order was placed") {
 		return true, "", false, nil
 	}
 
 	return false, "", false, nil
 }
 
-func (checkout *AmazonCheckout) amazonAddToCart(client *http.Client) (bool, string, string, bool, error) {
-	postData := fmt.Sprintf(`isAsync=1&asin.1=%s&offerListing.1=%s&quantity.1=1`, checkout.Sku, checkout.OfferId)
+func (checkout *AmazonCheckout) AmazonAddToCartV1(client *http.Client) (bool, string, string, bool, error) {
+	postData := fmt.Sprintf(`isAsync=1&asin.1=%s&quantity.1=1`, checkout.Sku)
 
 	var data = strings.NewReader(postData)
-	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/turbo-initiate?ref_=dp_start-bbf_1_glance_buyNow_2-1&referrer=detail&pipelineType=turbo&clientId=retailwebsite&weblab=RCX_CHECKOUT_TURBO_DESKTOP_PRIME_87783&temporaryAddToCart=1&asin.1=", data)
+	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/turbo-initiate?pipelineType=turbo", data)
 	if err != nil {
 		return false, "", "", false, err
 	}
 
-	req.Header.Set("x-amz-checkout-entry-referer-url", "https://www.amazon.com/dp/"+checkout.Sku)
-	req.Header.Set("x-amz-turbo-checkout-dp-url", "https://www.amazon.com/dp/"+checkout.Sku)
-	req.Header.Set("x-amz-checkout-csrf-token", checkout.SessionId)
-	req.Header.Set("user-agent", checkout.UserAgent)
-	req.Header.Set("cookie", checkout.Cookies)
+	req.Header = http.Header{
+		"accept":                           {"*/*"},
+		"accept-encoding":                  {"gzip, deflate, br"},
+		"accept-language":                  {"en,en_US;q=0.9"},
+		"content-length":                   {"40"},
+		"content-type":                     {"application/x-www-form-urlencoded"},
+		"cookie":                           {checkout.Cookies},
+		"device-memory":                    {"8"},
+		"downlink":                         {"3.9"},
+		"dpr":                              {"1"},
+		"ect":                              {"4g"},
+		"rtt":                              {"50"},
+		"sec-ch-device-memory":             {"8"},
+		"sec-ch-dpr":                       {"1"},
+		"sec-ch-ua":                        {checkout.Mimicer.ClientHintUA()},
+		"sec-ch-ua-mobile":                 {"?0"},
+		"sec-ch-ua-platform":               {`"Windows"`},
+		"sec-ch-viewport-width":            {`"988"`},
+		"sec-fetch-dest":                   {"empty"},
+		"sec-fetch-mode":                   {"cors"},
+		"sec-fetch-site":                   {"same-origin"},
+		"user-agent":                       {checkout.UserAgent},
+		"viewport-width":                   {"988"},
+		"x-amz-checkout-csrf-token":        {checkout.SessionId},
+		"x-amz-checkout-entry-referer-url": {"https://www.amazon.com/dp/" + checkout.Sku},
+		"x-amz-support-custom-signin":      {"1"},
+		"x-amz-turbo-checkout-dp-url":      {"https://www.amazon.com/dp/" + checkout.Sku},
+		"x-requested-with":                 {"XMLHttpRequest"},
+
+		http.HeaderOrderKey: {
+			"accept", "accept-encoding", "accept-language",
+			"content-length", "content-type", "cookie",
+			"device-memory", "downlink", "dpr",
+			"ect", "rtt", "sec-ch-device-memory",
+			"sec-ch-dpr", "sec-ch-ua", "sec-ch-ua-mobile", "sec-ch-ua-platform",
+			"sec-ch-viewport-width", "sec-fetch-dest", "sec-fetch-mode",
+			"sec-fetch-site", "user-agent", "viewport-width",
+			"x-amz-checkout-csrf-token", "x-amz-checkout-entry-referer-url",
+			"x-amz-support-custom-signin", "x-amz-turbo-checkout-dp-url",
+			"x-requested-with",
+		},
+		http.PHeaderOrderKey: checkout.Mimicer.PseudoHeaderOrder(),
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return false, "", "", false, err
@@ -117,9 +193,12 @@ func (checkout *AmazonCheckout) amazonAddToCart(client *http.Client) (bool, stri
 		return false, "", "", false, err
 	}
 
+	log.Println(string(bodyText))
+
 	if strings.Contains(string(bodyText), "Place your order") {
+		log.Println("Generated Checkout")
 		doc := soup.HTMLParse(string(bodyText))
-		purchaseId := elektra.Parse(string(bodyText), "currentPurchaseId\":\"", "\",\"pipelineType\"")
+		purchaseId := Parse(string(bodyText), "currentPurchaseId\":\"", "\",\"pipelineType\"")
 		csrfToken := doc.Find("input", "name", "anti-csrftoken-a2z").Attrs()["value"]
 		return true, purchaseId, csrfToken, false, nil
 	} else {
@@ -127,48 +206,27 @@ func (checkout *AmazonCheckout) amazonAddToCart(client *http.Client) (bool, stri
 	}
 }
 
-func (checkout *AmazonCheckout) amazonPrepareCart(client *http.Client) error {
-	var data = strings.NewReader(`isAsync=1&addressID=`)
-	req, err := http.NewRequest("POST", "https://www.amazon.com/checkout/turbo-initiate?ref_=chk_detail_eligibility_1-0&referrer=detail&pipelineType=turbo&clientId=retailwebsite&weblab=RCX_CHECKOUT_TURBO_DESKTOP_NONPRIME_87784&checkEligibilityOnly=true&temporaryAddToCart=1", data)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("x-amz-checkout-entry-referer-url", "https://www.amazon.com/dp/"+checkout.Sku)
-	req.Header.Set("x-amz-turbo-checkout-dp-url", "https://www.amazon.com/dp/"+checkout.Sku)
-	req.Header.Set("x-amz-checkout-csrf-token", checkout.SessionId)
-	req.Header.Set("user-agent", checkout.UserAgent)
-	req.Header.Set("cookie", checkout.Cookies)
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	return nil
-}
 
 func (checkout *AmazonCheckout) AmazonCheckoutTask() (bool, bool, error) {
-	checkout.Active = true
-	checkout.Id = uuid.New().String()
+	m, _ := mimic.Chromium(mimic.BrandChrome, latestVersion)
+	
+	checkout.Mimicer = *m
 
-	client, err := elektra.CreateClient(checkout.Proxy)
-	if err != nil {
-		return false, false, err
+	parsedCookies := cookieHeader(checkout.Cookies)
+
+	for _, cookie := range parsedCookies {
+		if cookie.Name == "session-id" {
+			checkout.SessionId = cookie.Value
+			break
+		}
 	}
 
-	if checkout.UserAgent == "" {
-		checkout.UserAgent = ua.RandomType(ua.Desktop) //If checkoutData.UserAgent is empty, set it to a randomly generated user agent
-	}
+	client := &http.Client{Transport: m.ConfigureTransport(&http.Transport{})}
 
 	for retries := 0; retries < checkout.MaxRetries; retries++ {
-		checkout.logMessage("Preparing Cart")
+		checkout.logMessage("Generating Checkout")
 		if !checkout.Active {return false, false, nil}
-		checkout.amazonPrepareCart(client)
-
-		checkout.logMessage("Adding to Cart")
-		if !checkout.Active {return false, false, nil}
-		cartSuccess, purchaseId, csrfToken, isBanned, err := checkout.amazonAddToCart(client)
+		cartSuccess, purchaseId, csrfToken, isBanned, err := checkout.AmazonAddToCartV1(client)
 		if err != nil {
 			return false, false, err
 		} else if isBanned {
@@ -178,9 +236,9 @@ func (checkout *AmazonCheckout) AmazonCheckoutTask() (bool, bool, error) {
 		}
 
 		if cartSuccess {
-			checkout.logMessage("Submitting Order")
+			checkout.logMessage("Placing Order")
 			if !checkout.Active {return false, false, nil}
-			success, orderNum, isBanned, err := checkout.amazonPlaceOrder(client, purchaseId, csrfToken) //Todo: add ability to fetch order number, currently returns empty string
+			success, orderNum, isBanned, err := checkout.AmazonPlaceOrderV1(client, purchaseId, csrfToken) //Todo: add ability to fetch order number, currently returns empty string
 			if err != nil {
 				return false, false, err
 			} else if isBanned {
@@ -191,6 +249,7 @@ func (checkout *AmazonCheckout) AmazonCheckoutTask() (bool, bool, error) {
 
 			if success {
 				checkout.OrderNum = orderNum
+				checkout.logMessage("Order Placed")
 				return true, false, nil
 			}
 		}
